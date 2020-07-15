706db47a9aadb442b5ddf3ca4756f97d2fd77bfc
Dylan Cromer <dylan@dylancromer.com>
2020-07-15 10:38:30 -0400

    MAINT: Refactored Rho

--- meso/_meso.py
+++ meso/_meso.py
@@ -3,11 +3,11 @@ import numpy as np
 import meso.mathutils as mathutils
 
 
-NUM_PHIS = 40
-
-
 @dataclass
 class Rho:
+    NUM_PHIS = 40
+
+    num_points: int = 200
     min_radius: float = 1e-4
     max_radius: float = 10
 
@@ -17,14 +17,19 @@ class Rho:
         phis = phis[None, None, :]
         return np.sqrt(radii**2 + ys**2 + 2*radii*ys*np.cos(phis))
 
-    def miscenter(self, radii, rho_func, prob_dist_func, num_points=200):
-        ys = np.logspace(
+    def _get_y_samples(self):
+        return np.logspace(
             np.log10(self.min_radius),
             np.log10(self.max_radius),
-            num_points,
+            self.num_points,
         )
 
-        phis = np.linspace(0, 2*np.pi, NUM_PHIS)
+    def _get_phi_samples(self):
+        return np.linspace(0, 2*np.pi, self.NUM_PHIS)
+
+    def _get_y_phi_integrand_and_differentials(self, radii, rho_func, prob_dist_func):
+        ys = self._get_y_samples()
+        phis = self._get_phi_samples()
 
         rho_arg = self._rho_arg(radii, ys, phis)
         rhos = rho_func(rho_arg)
@@ -32,7 +37,9 @@ class Rho:
             prob_dist_func(ys)[None, :, None],
             rhos.ndim,
         )
-        y_integrand = probs * rhos
+        return probs * rhos, np.gradient(ys), np.gradient(phis)
 
-        phi_integrand = mathutils.trapz_(y_integrand, 1, dx=np.gradient(ys))
-        return mathutils.trapz_(phi_integrand, 1, dx=np.gradient(phis)) / (2*np.pi)
+    def miscenter(self, radii, rho_func, prob_dist_func):
+        y_phi_integrand, dys, dphis = self._get_y_phi_integrand_and_differentials(radii, rho_func, prob_dist_func)
+        phi_integrand = mathutils.trapz_(y_phi_integrand, 1, dx=dys)
+        return mathutils.trapz_(phi_integrand, 1, dx=dphis) / (2*np.pi)
